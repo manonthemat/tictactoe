6de54926195676a2fff6a578ac7b52c781016019
JpSchoch <jason.p.schoch@gmail.com>
2014-08-25 14:53:43 -0700

    before firebase

--- js/main.js
+++ js/main.js
@@ -3,8 +3,23 @@ var tttApp = angular.module('tttApp', []);
 tttApp.controller('playerInfo', ['$scope', function($scope) {
 	$scope.playerName = '';
 
-var playerOne;
-var playerTwo;
+// programmatic representation of gameboard.  [['','',''], ['','',''], ['','','']] - each row on the board
+	$scope.cells = [['','',''], ['','',''], ['','','']];
+
+// may have to manually (bind) address each item with the ng-repeat methodology
+
+// define player one & two
+// create an object that defines color (part of define player)
+
+// define who is active
+
+// how does each turn move to the next one?  counter? true/false?
+
+// each click does something with the array.  integer/string?
+
+// win logic.  check win conditions on the board.  loops.  in row, in col, in diag.
+
+// start simple
 
 
 
