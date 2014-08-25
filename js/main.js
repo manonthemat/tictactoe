e9a9c0d17617036c2b874bcc42ee83a98ce4aa74
JpSchoch <jason.p.schoch@gmail.com>
2014-08-25 09:03:22 -0700

    adding weekend activity

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var tttApp = angular.module('tttApp', []);
+
+tttApp.controller('playerInfo', ['$scope', function($scope) {
+	$scope.playerName = '';
+
+var playerOne;
+var playerTwo;
+
+
+
+
+
+
+}]);
